0ce62705462b4b039b5826b150048ef617c92f64
ArtemDevAp <kulenko0984@gmail.com>
2022-02-16 22:40:22 +0300

    refactor adapter

--- app/src/main/java/com/artem/mi/spacexautenticom/ui/launchpad/LaunchpadAdapter.kt
+++ app/src/main/java/com/artem/mi/spacexautenticom/ui/launchpad/LaunchpadAdapter.kt
@@ -1,51 +1,42 @@
 package com.artem.mi.spacexautenticom.ui.launchpad
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.artem.mi.spacexautenticom.R
+import com.artem.mi.spacexautenticom.databinding.LaunchpadItemBinding
 import com.artem.mi.spacexautenticom.model.LaunchpadData
-import com.google.android.material.textview.MaterialTextView
 
 class LaunchpadAdapter(
     private val onItemSelect: (suiteId: String) -> Unit
 ) : ListAdapter<LaunchpadData, LaunchpadAdapter.ViewHolder>(LaunchpadDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.launchpad_item, parent, false)
-
-
-
-        return ViewHolder(view, onItemSelect)
+        val binding = LaunchpadItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), onItemSelect)
     }
 
     inner class ViewHolder(
-        itemView: View,
-        private val onItemSelect: (suiteId: String) -> Unit
-    ) : RecyclerView.ViewHolder(itemView) {
+        private val binding: LaunchpadItemBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
 
-        private val launchpadName: MaterialTextView = itemView.findViewById(R.id.launchpad_name)
+        fun bind(item: LaunchpadData, onItemSelect: (suiteId: String) -> Unit) =
+            with(binding) {
+                launchpadName.text = item.site_name_long
 
-        init {
-            itemView.setOnClickListener {
-                onItemSelect(getItem(absoluteAdapterPosition).site_id)
+                root.setOnClickListener {
+                    onItemSelect(item.site_id)
+                }
             }
-        }
-
-        fun bind(item: LaunchpadData) {
-            launchpadName.text = item.site_name_long
-
-        }
-
     }
 }
 
